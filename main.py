471b29ff3b57767d791c3443633c7ad580777f37
Neel Adwani <nprosofficial@gmail.com>
2021-07-05 02:58:21 +0000

    Added Data + Model

--- main.py
+++ main.py
@@ -4,24 +4,17 @@ import sklearn.metrics
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-data = pd.read_csv("data/heart.csv")
+data = pd.read_csv("data/fetal_health.csv")
 
-predictors = data[["age", "sex", "cp", "trtbps", "chol", "fbs", "restecg", "thalachh", "exng", "oldpeak", "slp", "caa", "thall"]]
-targets = data.output
+predictors = data[["baseline value", "accelerations", "fetal_movement", "uterine_contractions", "light_decelerations", "prolongued_decelerations", "histogram_width"]]
+targets = data.fetal_health
 pred_train, pred_test, tar_train, tar_test = train_test_split(predictors, targets, test_size = .4, random_state = 10)
-
-print(pred_train.shape)
-print(pred_test.shape)
-print(tar_train.shape)
-print(tar_test.shape)
 
 classifier = RandomForestClassifier(n_estimators = 300)
 classifier = classifier.fit(pred_train, tar_train)
 
 predictions = classifier.predict(pred_test)
 print(predictions[20])
-
-print(sklearn.metrics.confusion_matrix(tar_test, predictions))
 
 accuracy = sklearn.metrics.accuracy_score(tar_test, predictions)
 print(accuracy)
